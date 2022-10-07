afbb528a9c36c33a32fb138be30a3a98b9135443
Molly Wu <WuMengting93@outlook.com>
2022-10-07 09:53:53 +0800

    完成了个人资料模块

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
 $(function () {
     getUserInfo()
     // 点击按钮退出
-    $('#btnLogout').on('click',function(){
-        layer.confirm('确认退出吗?', {icon: 3, title:'提示'}, function(index){
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确认退出吗?', { icon: 3, title: '提示' }, function (index) {
             //do something
             // 清空本地的token值
             // localStorage.clear()  //清除本地所有的值
             localStorage.removeItem('Big_Event_token')
             location.href = '/login.html'
             layer.close(index);
-          })
+        })
     })
 })
 const layer = layui.layer
@@ -26,7 +26,7 @@ function getUserInfo() {
             if (res.code !== 0) return layer.msg(res.message)
             renderUserAvatar(res)
         },
-        error(err){
+        error(err) {
             console.log(err)     //responseJSON: {code: 1, message: '身份认证失败！'}
         }
     })
@@ -36,11 +36,10 @@ function renderUserAvatar(res) {
     const name = res.data.nickname || res.data.username
     if (res.data.user_pic) {
         $('.text-avatar').hide()
-        $('.user-box img').css('src', res.data.user_pic).show()
+        $('.layui-nav-img').attr('src', res.data.user_pic).show()
     } else {
         $('.layui-nav-img').hide()
         // 获取用户名的第一个字符
-        
         const char = name.charAt(0).toUpperCase()
         $('.text-avatar').html(char)
     }
--- assets/js/user_avatar.js
+++ assets/js/user_avatar.js
@@ -1,32 +1,55 @@
-const layer = layui.layer
+$(function () {
+    const layer = layui.layer
 
-// 1.1 获取裁剪区域的 DOM 元素
-var $image = $('#image')
-// 1.2 配置选项
-const options = {
-    // 纵横比
-    aspectRatio: 1,
-    // 指定预览区域
-    preview: '.img-preview'
-}
+    // 1.1 获取裁剪区域的 DOM 元素
+    var $image = $('#image')
+    // 1.2 配置选项
+    const options = {
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域
+        preview: '.img-preview'
+    }
 
-// 1.3 创建裁剪区域
-$image.cropper(options)
+    // 1.3 创建裁剪区域
+    $image.cropper(options)
 
-// 点击上传按钮上传文件
-$('#btnChoose').on('click', function () {
-    file.click()
-})
+    // 点击上传按钮上传文件
+    $('#btnChoose').on('click', function () {
+        file.click()
+    })
 
-$('#file').on('change', function (e) {
-    // console.dir(e)
-    const filelist = e.target.files
-    // 判断用户是否上传了文件
-    if (filelist.length === 0) return layer.msg('请选择上传的头像')
-    var newImgURL = URL.createObjectURL(filelist[0])
-    // console.log(newImgURL)
-    $image
-        .cropper('destroy')      // 销毁旧的裁剪区域
-        .attr('src', newImgURL)  // 重新设置图片路径
-        .cropper(options)        // 重新初始化裁剪区域
+    $('#file').on('change', function (e) {
+        // console.dir(e)
+        const filelist = e.target.files
+        // 判断用户是否上传了文件
+        if (filelist.length === 0) return layer.msg('请选择上传的头像')
+        var newImgURL = URL.createObjectURL(filelist[0])
+        // console.log(newImgURL)
+        $image
+            .cropper('destroy')      // 销毁旧的裁剪区域
+            .attr('src', newImgURL)  // 重新设置图片路径
+            .cropper(options)        // 重新初始化裁剪区域
+    })
+
+    $('#btnConfirm').on('click', function () {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        $.ajax({
+            type: 'PATCH',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success(res) {
+                if (res.code !== 0) return layer.msg(res.message)
+                layer.msg(res.message)
+                window.parent.getUserInfo()
+            }
+        })
+    })
 })
